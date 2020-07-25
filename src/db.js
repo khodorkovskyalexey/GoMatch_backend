ab6260775f6c812301307dc14499535471a8c26c
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-07-25 16:46:19 +0600

    add post /auth endpoint

---
+++ src/db.js
@@ -0,0 +1,23 @@
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const db = new Sequelize('postgres://xylbrwhd:jnXlIHXaBK0Xg_ufSomLyjygf7Muo7fv@arjuna.db.elephantsql.com:5432/xylbrwhd')
+
+const User = db.define('user', {
+    user_id: Sequelize.UUID,
+    name: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    middle_name: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    email: Sequelize.STRING,
+    avatar: Sequelize.STRING,
+    bio: Sequelize.TEXT,
+    mark: Sequelize.INTEGER,
+    counter_trip: Sequelize.INTEGER,
+})
+
+db.sync()
+
+module.exports = {
+    db,
+    User,
+}
---
+++ src/server.js
@@ -0,0 +1,51 @@
+const Koa = require('koa')
+const logger = require('koa-morgan')
+const router = require('koa-router')()
+const bodyParser = require("koa-body")()
+const { User } = require('./db')
+
+const server = new Koa()
+
+const { v4: uuidv4 } = require('uuid')
+
+const { Record } = require('./db')
+
+router
+    .post("/auth", bodyParser, async ctx => {
+        let pass_is_true = true
+        /*
+        тут надо проверить правильность пароля (кода из смс), который передается в ctx.request.body['code'],
+        если он неверный, то pass_is_true = false
+        */
+        if(pass_is_true) {
+            const token = uuidv4()
+            await User.
+                findOrCreate({ where: { phone: ctx.request.body['phone'] } })
+                .then(([user]) => {
+                    User.update({ user_id: token }, { where: { id: user.id } })
+                })
+            ctx.body = {
+                status: 201,
+                token: token
+            }
+        } else {
+            ctx.body = { status: 401 }
+        }
+        
+    })
+    .post('/', bodyParser, ctx => {
+        User.create({
+            name: ctx.request.body['name'],
+            phone: ctx.request.body['phone']
+        })
+        ctx.body = ctx.request.body
+    })
+    .get('/', async ctx => {
+        ctx.body = await User.findAll()
+    })
+
+server
+    .use(logger('tiny'))
+    .use(router.routes())
+    .listen(8080)
+console.log("server is starting...")
