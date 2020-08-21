20e6d43ac663ebf47333a05ef9823d64f9081a84
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-08-21 19:00:44 +0600

    fix bug

--- src/server.js
+++ src/server.js
@@ -13,6 +13,15 @@ const reviewRoutes = require("./routes/review")
 
 const port = process.env.PORT || 8080
 server
+    .use(async (ctx, next) => {
+        ctx.set("Access-Control-Allow-Origin", "*")
+        ctx.set(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        )
+        ctx.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+        await next()
+    })
     //routes
     .use(authRoutes.routes())
     .use(userRoutes.routes())
