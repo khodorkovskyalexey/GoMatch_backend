d6f669537a0eeabe2df2d0ea3cae12f13270b069
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-08-25 20:09:50 +0600

    fix?

--- src/routes/region.js
+++ src/routes/region.js
@@ -2,8 +2,8 @@ const router = require('koa-router')()
 const bodyParser = require("koa-body")()
 const { Region } = require('../db')
 
-router.get("/region", bodyParser, async ctx => {
-	ctx.body = await Region.findAll({ attributes: ["name"] })
+router.get("/region", async ctx => {
+	ctx.body = await Region.findAll()
 })
 
 module.exports = router
