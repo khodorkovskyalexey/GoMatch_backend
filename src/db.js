249586d6324b465ad566e161a8e25a7255ef3829
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-09-14 18:50:27 +0600

    add areas in regions

--- src/db.js
+++ src/db.js
@@ -65,4 +65,5 @@ module.exports = {
     Request,
     Region,
     Passenger,
+    Area,
 }
--- src/routes/region.js
+++ src/routes/region.js
@@ -1,9 +1,29 @@
 const router = require('koa-router')()
 const bodyParser = require("koa-body")()
-const { Region } = require('../db')
+const { Region, Area } = require('../db')
 
 router.get("/region", async ctx => {
-	ctx.body = await Region.findAll()
+	ctx.body = await Region.findAll({ attributes: ["name"] })
+})
+
+router.post("/region", bodyParser, async ctx => {
+	await Region.findOrCreate({ where: { name: ctx.request.body["name"] } })
+})
+
+router.post("/area", bodyParser, async ctx => {
+	await Area.findOrCreate({ where: {
+		name: ctx.request.body["name"],
+		region_name: ctx.request.body["region_name"]
+	} })
+})
+
+router.get("/area", async ctx => {
+	ctx.body = await Area.findAll()
+})
+
+router.get("/area/:region", bodyParser, async ctx => {
+	ctx.body = await Area.findAll({ where: { region_name: ctx.params["region"] },
+		attributes: ["region_name", "name"] })
 })
 
 module.exports = router
