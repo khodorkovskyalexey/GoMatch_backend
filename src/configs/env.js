030c9da1ef56d5798a77fa3a6977c0bd24e8718c
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-10-30 12:08:02 +0600

    refactor DB_CONN to DB_CONNECTION

--- src/configs/env.js
+++ src/configs/env.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
     TOKEN_NAMESPACE: process.env.TOKEN_NAMESPACE,
-    DB_CONN: process.env.DB_CONNECTION,
+    DB_CONNECTION: process.env.DB_CONN,
     DELTA_MINUTES_FOR_DEADLINE: process.env.DELTA_MINUTES_FOR_DEADLINE,
 }
